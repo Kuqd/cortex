cd4269e96a03ff2a2fde665e776eaf2943cc98f3
Owen Diehl <ow.diehl@gmail.com>
2019-10-21 17:56:10 -0400

    linting Signed-off-by: Owen Diehl <ow.diehl@gmail.com>
    Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- pkg/querier/queryrange/query_range.go
+++ pkg/querier/queryrange/query_range.go
@@ -21,9 +21,10 @@ import (
 	"github.com/cortexproject/cortex/pkg/ingester/client"
 )
 
+// Exported statuses for downstream queries
 const (
-	StatusSuccess = "success" // Successful status
-	StatusFailure = "failure" // Failure status
+	StatusSuccess = "success"
+	StatusFailure = "failure"
 )
 
 var (
